14c5866997f22f79dd482cedf377a8040e8dd384
Jabborovmuhammadyusuf <muhammadyusufsevgi@gmail.com>
2026-06-18 21:33:39 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@
 import os
 
 # Railway Variables dan o'qiydi, lokal testda to'g'ridan-to'g'ri yozing
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8989992960:AAHYcHWwzr3BkHhXsqSqpvn7bqXUhETl_NQ")
-ADMIN_ID  = int(os.environ.get("ADMIN_ID", "8427195901"))
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8989992960:AAHYCHWwzr3BkhHXsSqspvn7bqXUhET1_NQ")
+ADMIN_ID = int(os.environ.get("ADMIN_ID", "8427195901"))
