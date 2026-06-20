893480d54e0ef5f75ba0fe4167fd739da3186bbc
Jabborovmuhammadyusuf <muhammadyusufsevgi@gmail.com>
2026-06-20 11:57:21 +0500

    Fix import statement for add_coins
    Corrected the import statement for add_coins.

--- handlers/private_chat.py
+++ handlers/private_chat.py
@@ -130,7 +130,7 @@ from aiogram.filters import Command
 # config.py faylida ADMIN_ID o'zgaruvchisi bor deb hisoblaymiz
 from config import ADMIN_ID 
 # db.py faylidagi bazani yangilash funksiyasi (loyihangizga qarab nomlang)
-from database.db_main import add_coins
+from database.db_main import add_coins  # To'g'rilangan import
 
 
 router = Router()
